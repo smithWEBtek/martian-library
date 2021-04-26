28f9dc4521bc98e30b61df2a9892f4b8460fc70a
brad@smithwebtek.com <brad@smithwebtek.com>
2021-04-26 16:50:50 -0400

    Add user, and enriched query

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :full_name, String, null: false
+    field :email, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def full_name
+      # `object` references the user instance
+      [object.first_name, object.last_name].compact.join(" ")
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,26 +15,26 @@ Item.create!(
     {
       title: "Martian Chronicles",
       description: "Cult book by Ray Bradbury",
-      user: john,
+      user_id: john.id,
       image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg"
     },
     {
       title: "The Martian",
       description: "Novel by Andy Weir about an astronaut stranded on Mars trying to survive",
-      user: john,
+      user_id: john.id,
       image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg"
     },
     {
       title: "Doom",
       description: "A group of Marines is sent to the red planet via an ancient " \
                    "Martian portal called the Ark to deal with an outbreak of a mutagenic virus",
-      user: jane,
+      user_id: jane.id,
       image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg"
     },
     {
       title: "Mars Attacks!",
       description: "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor",
-      user: jane,
+      user_id: jane.id,
       image_url: "https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg"
     }
   ]

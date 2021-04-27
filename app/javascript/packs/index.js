7441500794d9866f26f2c0b596b98edb7aecaef0
brad@smithwebtek.com <brad@smithwebtek.com>
2021-04-27 10:25:27 -0400

    Persisting tokens

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 import Library from '../components/Library';
+import UserInfo from '../components/UserInfo';
 
 render(
   <Provider>
     <Library />
+    <UserInfo />
   </Provider>, 
   document.querySelector('#root')
 );
